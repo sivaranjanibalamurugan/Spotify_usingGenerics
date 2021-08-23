1a8d9dd93f5b73f6de7bf2c26613e367a99f3f69
Siva Ranjani <sivaranjani.b@tvsnext.io>
2021-08-23 22:24:45 +0530

    [siva]Refactoring : code using priority

---
+++ SpotifyUsingGenerics/SpotifyusingGenerics.cs
@@ -0,0 +1,141 @@
+/*
+ * Project = Checking the users details
+ * Author  = Siva Ranjani B
+ * Created Date = 18/08/2021
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Net;
+using Newtonsoft.Json;
+using SpotifyUsingGenerics;
+
+namespace RestSharpAPITesting
+{
+    [TestClass]
+    public class SpotifyApItesting
+    {
+        public static string myToken = "";
+        public static string userId = "";
+        public static string playlistId = "";
+        public static IRestResponse restResponse;
+        IRestClient restclient = new RestClient();
+
+        [TestInitialize]
+        public void setup()
+        {
+            myToken = Utility.GetToken();
+        }
+
+        [Priority(1)]
+        [TestMethod]
+        //Method to get current user details
+        public void CurrentUserUsingRestSharp()
+        {
+            //Get Url link
+            string geturl = "https://api.spotify.com/v1/me";
+
+            IRestRequest restRequest = Utility.RestRequestutility(geturl);
+            restResponse = restclient.Get(restRequest);
+            //AssertStatement
+            Assert.AreEqual(HttpStatusCode.OK, restResponse.StatusCode);
+            Utility.Responsemessage(restResponse);
+            //deseraialization object 
+            var output = JsonConvert.DeserializeObject<dynamic>(restResponse.Content);
+            userId = output.id;
+
+            System.Diagnostics.Debug.WriteLine("UserId:" + userId);
+        }
+        [Priority(2)]
+        [TestMethod]
+        //Method to get user profile
+        public void UserProfileUsingRestSharp()
+        {
+            //GetUrlfor geeting user profile
+            string getuserurl = "https://api.spotify.com/v1/users/" + userId + "/";
+
+            IRestRequest restRequest = Utility.RestRequestutility(getuserurl);
+            restResponse = restclient.Get(restRequest);
+
+            //Assert.AreEqual(HttpStatusCode.OK, restResponse.StatusCode);
+            Console.WriteLine((int)restResponse.StatusCode);
+            System.Diagnostics.Debug.WriteLine(restResponse.StatusCode);
+            Utility.Responsemessage(restResponse);
+            //deseraialization object 
+            var output = JsonConvert.DeserializeObject<dynamic>(restResponse.Content);
+            userId = output.id;
+
+            System.Diagnostics.Debug.WriteLine("userId:" + playlistId);
+
+        }
+
+        [Priority(3)]
+        [TestMethod]
+        //Method to create playlist
+        public void PostCreatePlaylist()
+        {
+            string postUrl = "https://api.spotify.com/v1/users/" + userId + "/playlists";
+
+
+            string JsonData = "{" +
+                            "\"name\": \"Soubarnika Playlist\"," +
+                            "\"description\": \"New playlist description\"," +
+                            "\"public\":\" false\"" +
+                          "}";
+            IRestRequest restRequest = Utility.RestRequestutility(postUrl);
+
+            restRequest.AddJsonBody(JsonData);
+            restResponse = restclient.Post(restRequest);
+
+            Assert.AreEqual(201, (int)restResponse.StatusCode);
+            Utility.Responsemessage(restResponse);
+            //deseraialization object 
+            var output = JsonConvert.DeserializeObject<dynamic>(restResponse.Content);
+            playlistId = output["id"];
+            System.Diagnostics.Debug.WriteLine("PlaylistId:" + playlistId);
+        }
+
+        //Method to update playlist
+        [Priority(4)]
+        [TestMethod]
+        public void U_pdatePLaylist()
+        {
+            //Url for PUT method
+            string putUrl = "https://api.spotify.com/v1/playlists/" + playlistId + "/";
+            //Json Body
+            string JsonData = "{" +
+                              "\"name\": \" Playlist soubarnika\"," +
+                              "\"description\": \"New playlist description\"," +
+                              "\"public\": false" +
+                            "}";
+            //Interfaces 
+            IRestClient restClient = new RestClient();
+            IRestRequest restRequest = new RestRequest(putUrl);
+
+            restRequest.AddHeader("Authorization", "token" + myToken);
+            restRequest.AddJsonBody(JsonData);
+            restResponse = restClient.Put(restRequest);
+            //Assertion statement
+            Assert.AreEqual(200, (int)restResponse.StatusCode);
+            Utility.Responsemessage(restResponse);
+
+        }
+
+        //Method to delete playlist
+        [TestMethod]
+        public void XDeletePlaylist()
+        {
+            string DelUrl = "https://api.spotify.com/v1/playlists/" + playlistId + "/";
+
+            IRestRequest restRequest = Utility.RestRequestutility(DelUrl);
+            restResponse = restclient.Delete(restRequest);
+            Assert.AreEqual(HttpStatusCode.OK, restResponse.StatusCode);
+
+            Utility.Responsemessage(restResponse);
+        }
+
+
+
+    }
+}
---
+++ SpotifyUsingGenerics/Utility.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using System;
+/*
+ * Project = Checking the users details
+ * Author  = Siva Ranjani B
+ * Created Date = 21/08/2021
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyUsingGenerics
+{
+    public class Utility
+    { 
+            //Method to get token 
+            public static string GetToken()
+            {
+                string myToken = "Bearer BQD3WXPcRAGkQEbolmoQKr6kfUbHg1a0pbBJnZ96IhiZ1zx_UzAciP1eaLHkPZp9GMbjzMCXxa4OGP4VKxmkCi7zgZO2LxQz-X7gK0DJRFA7Vv3yeznvQIlKx8SqxuS2wyazuVXvkD41L1GhXmJUeYeVhUhiXXD5GkUb-EZZYEf6Oc-fPLyBHZ1tolFodd3nPLZcjyX8YZZ7vJIaCKKoUABQNXJcYyieF_FPiwZF73pMLcBkG9oEdL0bCEBrEZzV7ELJ9P021rMykL27M-3cTdXpLV2LLhVhiS248Aud";
+                return myToken;
+            }
+            //RestRequest utility method
+            public static IRestRequest RestRequestutility(string url)
+            {
+
+                IRestRequest restRequest = new RestRequest(url);
+
+                restRequest.AddHeader("Authorization", "Token" + GetToken());
+
+                return restRequest;
+            }
+            //SUccessful response method
+            public static void Responsemessage(IRestResponse restResponse)
+            {
+                if (restResponse.IsSuccessful)
+                {
+                    Console.WriteLine("Statuscode:" + restResponse.StatusCode);
+                    Console.WriteLine("Response:" + restResponse.Content);
+                }
+                Console.WriteLine(restResponse.ErrorMessage);
+                Console.WriteLine(restResponse.ErrorException);
+            }
+    }
+}
+
+
